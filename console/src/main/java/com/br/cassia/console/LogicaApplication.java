d03e2c27c822a9cdbed4a32d3ccddfc3b41f31cb
cassialeaal <cassialeaal@gmail.com>
2022-08-22 20:04:07 -0300

    iniciando aplicacao spring boot e java com linhas de comando

---
+++ console/src/main/java/com/br/cassia/console/LogicaApplication.java
@@ -0,0 +1,17 @@
+package com.br.cassia.console;
+
+//import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicaApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(LogicaApplication.class, args);
+		System.out.println("================================");
+		System.out.println("Bem vindo ao app java console!!");
+		System.out.println("================================");
+
+	}
+
+}
